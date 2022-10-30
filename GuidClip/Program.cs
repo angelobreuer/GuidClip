a314156041adb5268606ea45448555575447f30b
angelobreuer <angelobr@outlook.de>
2022-10-30 19:57:20 +0100

    Initial Commit

---
+++ GuidClip/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using GuidClip;
+
+const int CF_UNICODETEXT = 0x0D;
+
+unsafe
+{
+    var buffer = (byte*)NativeMemory.Alloc(32 * 2 + 2);
+
+    var bytesWritten = Encoding.Unicode.GetBytes(
+        chars: Guid.NewGuid().ToString(),
+        bytes: new Span<byte>(buffer, 36 * 2));
+
+    Debug.Assert(bytesWritten is 36 * 2);
+
+    buffer[72] = 0; // Null Terminator
+    buffer[73] = 0; // Null Terminator
+
+    var result = SafeNativeMethods.OpenClipboard(IntPtr.Zero);
+    Debug.Assert(result);
+
+    result = SafeNativeMethods.EmptyClipboard();
+    Debug.Assert(result);
+
+    SafeNativeMethods.SetClipboardData(CF_UNICODETEXT, buffer);
+
+    result = SafeNativeMethods.CloseClipboard();
+    Debug.Assert(result);
+
+    NativeMemory.Free(buffer);
+}
---
+++ GuidClip/SafeNativeMethods.cs
@@ -0,0 +1,21 @@
+﻿namespace GuidClip;
+
+using System.Runtime.InteropServices;
+
+public static unsafe partial class SafeNativeMethods
+{
+    [return: MarshalAs(UnmanagedType.Bool)]
+    [LibraryImport("user32.dll", SetLastError = true)]
+    public static partial bool CloseClipboard();
+
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool EmptyClipboard();
+
+    [return: MarshalAs(UnmanagedType.Bool)]
+    [LibraryImport("user32.dll", SetLastError = true)]
+    public static partial bool OpenClipboard(IntPtr hWndNewOwner);
+
+    [LibraryImport("user32.dll")]
+    public static partial IntPtr SetClipboardData(uint uFormat, byte* memory);
+}
